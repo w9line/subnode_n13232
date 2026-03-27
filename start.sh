e33d6e43512ac7456577b67a161b940d3a6de48d
Wers <112427364+w9line@users.noreply.github.com>
2026-03-27 20:52:06 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 ./client -server="$SERVER" -session-id="$SESSION_ID" -mode="$MODE" -log="$LOG" &
-echo "client PID: $!"
 
 ./proxy &
-echo "proxy PID: $!"
 
 exec gost -L "socks5+wss://${GOST_USER}:${GOST_PASS}@:10000"
