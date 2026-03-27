2e9f972c1c82cd0e2f3e8a55a18c928e5046cd4d
Wers <112427364+w9line@users.noreply.github.com>
2026-03-27 20:48:02 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Запускаем клиент в фоне
 ./client -server="$SERVER" -session-id="$SESSION_ID" -mode="$MODE" -log="$LOG" &
-echo "Client started with PID $!"
+echo "client PID: $!"
 
-# Запускаем прокси в фоне
 ./proxy &
-echo "Proxy started with PID $!"
+echo "proxy PID: $!"
 
-# Ждем пару секунд, чтобы фоновые процессы успели стартовать
-sleep 2
-
-# Запускаем gost как основной процесс (через exec)
-# Важно: убедись, что gost.yaml лежит в /app/
-exec gost -C /app/gost.yaml
+exec gost -L "socks5+wss://${GOST_USER}:${GOST_PASS}@:10000"
